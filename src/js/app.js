40eafc0365ea2b442d9ae77a94ec3d8ad44db0b8
Khalid Saifudidn <khalid.saifuddin354@gmail.com>
2020-05-14 10:26:03 +0700

    terpaksa

--- src/js/app.js
+++ src/js/app.js
@@ -29,8 +29,9 @@ localStorage.setItem('google_api','1010108449023-6qolnf5bdkckntc94vq88brt28pgu04
 // localStorage.setItem('google_api','188472097829-q54nne8p4mdp0he19pivpg1sf6htd04e.apps.googleusercontent.com');
 // localStorage.setItem('api_base','http://localhost:8000/');
 // localStorage.setItem('api_base','http://117.53.44.48:8080/');
-localStorage.setItem('api_base','http://117.53.44.48:8080');
-// localStorage.setItem('api_base','http://ppdb:8888');
+// localStorage.setItem('api_base','http://117.53.47.95:8000');
+// localStorage.setItem('api_base','http://117.53.44.48:8080');
+localStorage.setItem('api_base','http://ppdb:8888');
 // localStorage.setItem('google_api','188472097829-4h5peopg70ndp9g1p9seg1abgkg64ot4.apps.googleusercontent.com');
 
 // // 026100
@@ -48,13 +49,20 @@ localStorage.setItem('api_base','http://117.53.44.48:8080');
 // localStorage.setItem('logo_aplikasi',"https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png");
 // localStorage.setItem('socket_url',"http://localhost:5000");
 
-// localStorage.setItem('judul_aplikasi','PPDB Lumajang');
-// localStorage.setItem('sub_judul_aplikasi','PPDB Lumajang');
+localStorage.setItem('judul_aplikasi','PPDB Lumajang');
+localStorage.setItem('sub_judul_aplikasi','PPDB Lumajang');
+localStorage.setItem('wilayah_aplikasi','Kabupaten Lumajang');
+localStorage.setItem('kode_wilayah_aplikasi','052100');
+localStorage.setItem('id_level_wilayah_aplikasi','2');
+localStorage.setItem('sudah_pilih_kota','1');
+localStorage.setItem('logo_wilayah','https://upload.wikimedia.org/wikipedia/commons/f/f4/Lambang_Kabupaten_Lumajang.png');
+localStorage.setItem('lintang_aplikasi','-8.109038');
+localStorage.setItem('bujur_aplikasi','113.141552');
+localStorage.setItem('zoom_aplikasi','10');
+
+//////
 localStorage.setItem('kode_aplikasi','PPDB');
 localStorage.setItem('tema_warna_aplikasi','biru-1');
-// localStorage.setItem('wilayah_aplikasi','Kabupaten Lumajang');
-// localStorage.setItem('kode_wilayah_aplikasi','052100');
-// localStorage.setItem('id_level_wilayah_aplikasi','2');
 localStorage.setItem('jenjang_aplikasi','5-6-13-15-29'); // 5=SD, 6=SMP, 13=SMA, 15=SMK, 29=SLB, 1=PAUD
 localStorage.setItem('semester_id_aplikasi','20192'); // 5=SD, 6=SMP, 13=SMA, 15=SMK, 29=SLB, 1=PAUD
 localStorage.setItem('versi_aplikasi','2020.05.01');
---
+++ www/precache-manifest.acdf3615635adac92b256b082b554a75.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9010c5b93175551e5ac4cd335c6a634e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9ee96e3a07dec6235d3f1a86787a92b7",
+    "url": "fonts/Framework7Icons-Regular.eot"
+  },
+  {
+    "revision": "3b973a05c0e1544f7ee8fa23c2a9315a",
+    "url": "fonts/Framework7Icons-Regular.ttf"
+  },
+  {
+    "revision": "535bcf7dd3feb41ec20507c3c5b81efb",
+    "url": "fonts/Framework7Icons-Regular.woff"
+  },
+  {
+    "revision": "1dfb73e22b9119f547fb880568f56ea2",
+    "url": "fonts/Framework7Icons-Regular.woff2"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "fonts/MaterialIcons-Regular.eot"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "fonts/MaterialIcons-Regular.ttf"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "fonts/MaterialIcons-Regular.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "fonts/MaterialIcons-Regular.woff2"
+  },
+  {
+    "revision": "57349c420cec79d36bc3",
+    "url": "js/app.js"
+  },
+  {
+    "revision": "6142ab3de14e65b74a79ea7a352d452f",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b08f600e75284d6274ee0ee03f2ed1b3",
+    "url": "static/icons/128x128.png"
+  },
+  {
+    "revision": "78bb1bad56b56f9176976fbb19f640d8",
+    "url": "static/icons/144x144.png"
+  },
+  {
+    "revision": "09bac65cbc73f494482c480006f7c31a",
+    "url": "static/icons/152x152.png"
+  },
+  {
+    "revision": "1c41296078623fe115c3b966852a9759",
+    "url": "static/icons/192x192.png"
+  },
+  {
+    "revision": "ad809d0a19d880fc189e41e9726df58e",
+    "url": "static/icons/256x256.png"
+  },
+  {
+    "revision": "9fa40ac188452505f216044f8c7a9b88",
+    "url": "static/icons/512x512.png"
+  },
+  {
+    "revision": "14235e3018bb57cc700c799c5f9ef1be",
+    "url": "static/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "c88219dd9796cca0bfa90e472a805ab9",
+    "url": "static/icons/favicon.png"
+  },
+  {
+    "revision": "0de87da05b0352951dc5c27e76364803",
+    "url": "static/icons/xls.png"
+  },
+  {
+    "revision": "b2c228a3bdd24c7c3486841b57343d5e",
+    "url": "static/images/formulir-illustration.png"
+  },
+  {
+    "revision": "df3dcb927ee9282f88d86510a49ebf2e",
+    "url": "static/images/icons/afirmasi.svg"
+  },
+  {
+    "revision": "20bc7f469347709dce96011b21acc8b7",
+    "url": "static/images/icons/cari-data.svg"
+  },
+  {
+    "revision": "7e996877c60014d4eae55196a7b86e6a",
+    "url": "static/images/icons/formulir.svg"
+  },
+  {
+    "revision": "ce6a759a666f5fc86a0f27ff9205815d",
+    "url": "static/images/icons/jadwal.svg"
+  },
+  {
+    "revision": "b8c0b69b150cb4d363c4f6477db6bf7e",
+    "url": "static/images/icons/kuota.svg"
+  },
+  {
+    "revision": "648efd92732dfaca367bf5c0c700704d",
+    "url": "static/images/icons/pengumuman.svg"
+  },
+  {
+    "revision": "571ff3201560bd8c598ae167131c74ea",
+    "url": "static/images/icons/perpindahan-ortu.svg"
+  },
+  {
+    "revision": "3ce90405d0c5af9a113a892df9cf48d4",
+    "url": "static/images/icons/petunjuk.svg"
+  },
+  {
+    "revision": "299dd6e38d06c7c743c20f11f4ba5c5f",
+    "url": "static/images/icons/prestasi.svg"
+  },
+  {
+    "revision": "6101df9cd8efd332bc05801ef7716915",
+    "url": "static/images/icons/zonasi.svg"
+  },
+  {
+    "revision": "49d92db8a6990d34a43b17adcd050ed1",
+    "url": "static/images/logo-kabupaten-lumajang.png"
+  }
+]);
