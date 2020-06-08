a727e862151f3367986ac4647ced87fe4917b7ef
Khalid Saifudidn <khalid.saifuddin354@gmail.com>
2020-06-08 13:30:54 +0700

    app

--- src/js/app.js
+++ src/js/app.js
@@ -55,8 +55,23 @@ Framework7.use(Framework7React)
 // LUMAJANG
 // ====================================
 
-// localStorage.setItem('api_base','http://117.53.47.95:8000');
-// localStorage.setItem('google_api','1010108449023-6qolnf5bdkckntc94vq88brt28pgu04n.apps.googleusercontent.com'); // lumajang
+localStorage.setItem('api_base','http://117.53.47.95:8000');
+localStorage.setItem('google_api','1010108449023-6qolnf5bdkckntc94vq88brt28pgu04n.apps.googleusercontent.com'); // lumajang
+
+// localStorage.setItem('judul_aplikasi','Hasil Seleksi PPDB Lumajang');
+// localStorage.setItem('sub_judul_aplikasi','Dinas Pendidikan Kabupaten Lumajang');
+// localStorage.setItem('wilayah_aplikasi','Kabupaten Lumajang');
+// localStorage.setItem('kode_wilayah_aplikasi','052100');
+// localStorage.setItem('id_level_wilayah_aplikasi','2');
+// localStorage.setItem('sudah_pilih_kota','1');
+// localStorage.setItem('publik','1');
+// localStorage.setItem('logo_wilayah','https://upload.wikimedia.org/wikipedia/commons/f/f4/Lambang_Kabupaten_Lumajang.png');
+// localStorage.setItem('lintang_aplikasi','-8.109038');
+// localStorage.setItem('bujur_aplikasi','113.141552');
+// localStorage.setItem('zoom_aplikasi','10');
+// localStorage.setItem('kode_aplikasi','PPDB-publik');
+// localStorage.setItem('root_base','/');
+// localStorage.setItem('tema_warna','ungu-terong');
 
 // localStorage.setItem('judul_aplikasi','PPDB Lumajang');
 // localStorage.setItem('sub_judul_aplikasi','Dinas Pendidikan Kabupaten Lumajang');
@@ -97,8 +112,8 @@ localStorage.setItem('lintang_aplikasi','-8.109038');
 localStorage.setItem('bujur_aplikasi','113.141552');
 localStorage.setItem('zoom_aplikasi','10');
 localStorage.setItem('kode_aplikasi','PPDB-sekolah');
-// localStorage.setItem('root_base','/sekolah/');
-localStorage.setItem('root_base','/');
+localStorage.setItem('root_base','/sekolah/');
+// localStorage.setItem('root_base','/');
 localStorage.setItem('tema_warna','ungu-terong');
 
 // ====================================
@@ -112,7 +127,7 @@ localStorage.setItem('tema_warna','ungu-terong');
 // // ====================================
 
 // localStorage.setItem('api_base','http://117.53.47.43:8080');
-// // localStorage.setItem('google_api','1010108449023-hkjibdo11irpvod6v3mfvitgdf4qm8rr.apps.googleusercontent.com'); // yang baru maluku tengah
+// localStorage.setItem('google_api','1010108449023-hkjibdo11irpvod6v3mfvitgdf4qm8rr.apps.googleusercontent.com'); // yang baru maluku tengah
 
 // localStorage.setItem('judul_aplikasi','PPDB Maluku Tengah');
 // localStorage.setItem('sub_judul_aplikasi','Dinas Pendidikan Kabupaten Maluku Tengah');
@@ -128,26 +143,42 @@ localStorage.setItem('tema_warna','ungu-terong');
 // localStorage.setItem('root_base','/');
 // localStorage.setItem('tema_warna','toska-lumut');
 
-// // localStorage.setItem('judul_aplikasi','PPDB Lumajang');
-// // localStorage.setItem('sub_judul_aplikasi','Dasbor Dinas');
-// // localStorage.setItem('wilayah_aplikasi','Kabupaten Lumajang');
-// // localStorage.setItem('kode_wilayah_aplikasi','052100');
-// // localStorage.setItem('id_level_wilayah_aplikasi','2');
-// // localStorage.setItem('sudah_pilih_kota','1');
-// // localStorage.setItem('logo_wilayah','https://upload.wikimedia.org/wikipedia/commons/f/f4/Lambang_Kabupaten_Lumajang.png');
-// // localStorage.setItem('lintang_aplikasi','-8.109038');
-// // localStorage.setItem('bujur_aplikasi','113.141552');
-// // localStorage.setItem('zoom_aplikasi','10');
-// // localStorage.setItem('kode_aplikasi','PPDB-dinas');
+// localStorage.setItem('judul_aplikasi','PPDB Maluku Tengah');
+// localStorage.setItem('sub_judul_aplikasi','Dasbor Dinas');
+// localStorage.setItem('wilayah_aplikasi','Kabupaten Maluku Tengah');
+// localStorage.setItem('kode_wilayah_aplikasi','210100');
+// localStorage.setItem('id_level_wilayah_aplikasi','2');
+// localStorage.setItem('sudah_pilih_kota','1');
+// localStorage.setItem('logo_wilayah','https://upload.wikimedia.org/wikipedia/commons/3/3d/Lambang_Kabupaten_Maluku_Tengah.png');
+// localStorage.setItem('lintang_aplikasi','-3.140835');
+// localStorage.setItem('bujur_aplikasi','129.260976');
+// localStorage.setItem('zoom_aplikasi','10');
+// localStorage.setItem('kode_aplikasi','PPDB-dinas');
 // // localStorage.setItem('root_base','/dinas/');
-// // localStorage.setItem('tema_warna','ungu-terong');
+// localStorage.setItem('root_base','/');
+// localStorage.setItem('tema_warna','toska-lumut');
+
+// localStorage.setItem('judul_aplikasi','PPDB Maluku Tengah');
+// localStorage.setItem('sub_judul_aplikasi','Dasbor Sekolah');
+// localStorage.setItem('wilayah_aplikasi','Kabupaten Maluku Tengah');
+// localStorage.setItem('kode_wilayah_aplikasi','210100');
+// localStorage.setItem('id_level_wilayah_aplikasi','2');
+// localStorage.setItem('sudah_pilih_kota','1');
+// localStorage.setItem('logo_wilayah','https://upload.wikimedia.org/wikipedia/commons/3/3d/Lambang_Kabupaten_Maluku_Tengah.png');
+// localStorage.setItem('lintang_aplikasi','-3.140835');
+// localStorage.setItem('bujur_aplikasi','129.260976');
+// localStorage.setItem('zoom_aplikasi','10');
+// localStorage.setItem('kode_aplikasi','PPDB-sekolah');
+// // localStorage.setItem('root_base','/sekolah/');
+// localStorage.setItem('root_base','/');
+// localStorage.setItem('tema_warna','toska-lumut');
 
 // // ====================================
 // // END OF MALUKU TENGAH
 // // ====================================
 
-localStorage.setItem('google_api','1010108449023-6qolnf5bdkckntc94vq88brt28pgu04n.apps.googleusercontent.com'); // lumajang
-localStorage.setItem('api_base','http://ppdb:8888');
+// localStorage.setItem('google_api','1010108449023-6qolnf5bdkckntc94vq88brt28pgu04n.apps.googleusercontent.com'); // lumajang
+// localStorage.setItem('api_base','http://ppdb:8888');
 
 //////
 // localStorage.setItem('kode_aplikasi','PPDB');
